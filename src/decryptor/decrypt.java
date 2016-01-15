992a6b0ecf9cab2a4f1d40b8ec3e6f1ae3747174
igor <igor@localhost.localdomain>
2016-01-15 17:29:13 +0600

    Created license.txt with computer unique information for key generating

--- src/decryptor/decrypt.java
+++ src/decryptor/decrypt.java
@@ -6,10 +6,26 @@
 
 package decryptor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
  *
  * @author igor
  */
 public class decrypt {
+    private String pathToFile;
+    private String pass;
     
+    public decrypt(String pathToFile, String pass){
+        this.pathToFile = pathToFile;
+        this.pass = pass;
+    }
+    
+    private String readMessage() {
+        try{
+        FileInputStream stream = new FileInputStream(pathToFile);
+        catch(FileNotFound)
+    }
 }
